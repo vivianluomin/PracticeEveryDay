ed26b60a76238a4c2feb499080b32756aeaa08c0
vivianluomin <273104241@qq.com>
2018-03-12 21:52:18 +0800

    把Unity导入AS

---
+++ UnityToIn/app/src/main/java/com/vivian/rollaball/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vivian.rollaball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main2);
+    }
+}
