5d4e97238e39fef5e4e236b0a44e03ca741fbb3c
vivianluomin <vivianluomin@gmail.com>
2018-05-24 23:12:22 +0800

    使用OPenGL进行相机预览

---
+++ CameraWithOpenGL/app/src/main/java/com/example/asus1/camerawithopengl/GLCameraPreview.java
@@ -0,0 +1,9 @@
+package com.example.asus1.camerawithopengl;
+
+import android.opengl.GLSurfaceView;
+
+public class GLCameraPreview  {
+
+    public GLCameraPreview(int cameraId) {
+    }
+}
---
+++ CameraWithOpenGL/app/src/main/java/com/example/asus1/camerawithopengl/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.asus1.camerawithopengl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       Constant.mScreenWdith =  getWindow().getDecorView().getWidth();
+       Constant.mScreenWdith = getWindow().getDecorView().getHeight();
+
+    }
+}
