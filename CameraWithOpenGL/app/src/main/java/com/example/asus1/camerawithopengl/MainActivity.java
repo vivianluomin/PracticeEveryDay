7b5340f73c282e7e414549a02813c538af2b431c
vivianluomin <273104241@qq.com>
2018-05-27 21:54:06 +0800

    试试github

--- CameraWithOpenGL/app/src/main/java/com/example/asus1/camerawithopengl/MainActivity.java
+++ CameraWithOpenGL/app/src/main/java/com/example/asus1/camerawithopengl/MainActivity.java
@@ -26,13 +26,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        mGLSurfaceView = new CameraGLView(this);
-//        mGLSurfaceView.setEGLContextClientVersion(2);
-//        mGLSurfaceView.setRenderer(new CameraRenderer(mGLSurfaceView));
-        GLSurfaceView surfaceView = new GLSurfaceView(this);
-        surfaceView.setEGLContextClientVersion(2);
-        surfaceView.setRenderer(new MyRenderer());
-        setContentView(surfaceView);
+        mGLSurfaceView = new CameraGLView(this);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView.setRenderer(new CameraRenderer(mGLSurfaceView));
+        setContentView(mGLSurfaceView);
 
 
     }
