1e7c3db2c86b52264bd137d57d7ce52637596ec1
vivianluomin <273104241@qq.com>
2018-08-09 22:55:40 +0800

    OpenGL学习之纹理贴图

---
+++ LearnOpenGL/app/src/main/java/com/example/asus1/learnopengl/shaders/TextureView.java
@@ -0,0 +1,92 @@
+package com.example.asus1.learnopengl.shaders;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
+import android.util.AttributeSet;
+
+import com.example.asus1.learnopengl.R;
+
+import java.io.InputStream;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class TextureView extends GLSurfaceView {
+
+    private TextureRender mRender;
+
+    public TextureView(Context context) {
+        this(context,null);
+    }
+
+    public TextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setEGLContextClientVersion(3);
+        mRender = new TextureRender();
+        setRenderer(mRender);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+    }
+
+    private class TextureRender implements Renderer{
+
+        private TriangleTexture mTriangle;
+        private int mTextureId;
+
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            mTriangle = new TriangleTexture();
+            GLES30.glClearColor(1,1,1,1);
+            initTexture();
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            GLES30.glViewport(0,0,width,height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+            mTriangle.drawSelf(mTextureId);
+        }
+
+        private void initTexture(){
+            int textures[] = new int[1]; //生成纹理id
+            GLES30.glGenTextures(
+                    1, //产生纹理id的数量
+                    textures, //纹理id的数组
+                    0  //偏移量
+                    );
+            mTextureId = textures[0];
+
+            GLES30.glBindTexture(GLES30.GL_TEXTURE_2D,mTextureId);//绑定纹理id
+
+            GLES30.glTexParameterf(GLES30.GL_TEXTURE_2D,
+                    GLES30.GL_TEXTURE_MIN_FILTER,GLES30.GL_NEAREST);//设置MIN 采样方式
+            GLES30.glTexParameterf(GLES30.GL_TEXTURE_2D,
+                    GLES30.GL_TEXTURE_MAG_FILTER,GLES30.GL_LINEAR);//设置MAG采样方式
+            GLES30.glTexParameterf(GLES30.GL_TEXTURE_2D,
+                    GLES30.GL_TEXTURE_WRAP_S,GLES30.GL_CLAMP_TO_EDGE);//设置S轴拉伸方式
+            GLES30.glTexParameterf(GLES30.GL_TEXTURE_2D,
+                    GLES30.GL_TEXTURE_WRAP_T,GLES30.GL_CLAMP_TO_EDGE);//设置T轴拉伸方式
+
+            //加载图片
+            Bitmap bitmap = BitmapFactory.decodeResource(
+                    TextureView.this.getResources(), R.mipmap.brick);
+            GLUtils.texImage2D( //实际加载纹理进显存
+                    GLES30.GL_TEXTURE_2D, //纹理类型
+                    0, //纹理的层次，0表示基本图像层，可以理解为直接贴图
+                    bitmap, //纹理图像
+                    0 //纹理边框尺寸
+            );
+
+            bitmap.recycle();
+
+        }
+    }
+}
---
+++ LearnOpenGL/app/src/main/java/com/example/asus1/learnopengl/shaders/TriangleTexture.java
@@ -0,0 +1,89 @@
+package com.example.asus1.learnopengl.shaders;
+
+import android.opengl.GLES30;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+public class TriangleTexture {
+
+    private  String mVertexShader=
+            "attribute vec3 aPosition;" + //顶点位置
+                    "attribute vec2 aTexCoord;" + //顶点纹理位置
+                    "varying vec2 vTextureCoord;" + //用于传递给片元着色器的易变变量
+                    "void main(){" +
+                    "gl_Position = vec4(aPosition,1);" +
+                    "vTextureCoord = aTexCoord;" +
+                    "}";
+    private String mFragmentShader =
+            "precision mediump float;" +
+                    "varying vec2 vTextureCoord;" +
+                    "uniform sampler2D sTexture;" + //纹理采样器，代表一副纹理
+                    "void main(){" +
+                    "gl_FragColor = texture2D(sTexture,vTextureCoord);" +//进行纹理采样
+                    "}";
+
+    private FloatBuffer mVertexBuffer;//顶点坐标数据缓冲
+    private FloatBuffer mTexCoordBuffer;//顶点纹理坐标数据缓冲
+
+    private int mvCount = 0;//顶点数量
+
+    private int maPositionHandle;
+    private int maTexCoordHandle;
+    private int mProgram;
+
+    public TriangleTexture(){
+        initVertext();
+        initShder();
+    }
+
+    private void  initVertext(){
+        mvCount = 3;
+
+        mvCount = 3;
+        float vertices[] = new float[]{
+                0,1,0,-1,0,0,1,0,0
+        };//顶点位置
+
+        ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length*4);
+        vbb.order(ByteOrder.nativeOrder());//设置字节顺序为本地操作系统顺序
+        mVertexBuffer = vbb.asFloatBuffer();
+        mVertexBuffer.put(vertices);//在缓冲区内写入数据
+        mVertexBuffer.position(0);//设置缓冲区的起始位置
+
+        float[] colors = new float[]{
+                0.5f,0,
+                0,1,
+                1,1
+        };//顶点颜色数组
+        ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length*4);
+        cbb.order(ByteOrder.nativeOrder());
+        mTexCoordBuffer = cbb.asFloatBuffer();
+        mTexCoordBuffer.put(colors);
+        mTexCoordBuffer.position(0);
+    }
+
+    private void initShder(){
+        mProgram = shaderUtil.createProgram(mVertexShader,mFragmentShader);
+        maPositionHandle = GLES30.glGetAttribLocation(mProgram,"aPosition");
+        maTexCoordHandle = GLES30.glGetAttribLocation(mProgram,"aTexCoord");
+    }
+
+    public void drawSelf(int textId){
+
+        GLES30.glUseProgram(mProgram);
+
+        GLES30.glVertexAttribPointer(maPositionHandle,3,
+                GLES30.GL_FLOAT,false,3*4,mVertexBuffer);
+        GLES30.glVertexAttribPointer(maTexCoordHandle,2,
+                GLES30.GL_FLOAT,false,2*4,mTexCoordBuffer);
+        GLES30.glEnableVertexAttribArray(maPositionHandle);
+        GLES30.glEnableVertexAttribArray(maTexCoordHandle);
+
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE0); //设置使用的纹理编号
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D,textId); //绑定指定的纹理id
+
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES,0,mvCount);
+    }
+}
