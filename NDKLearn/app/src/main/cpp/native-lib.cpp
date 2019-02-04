9cab30c08da76585c4fc2c54feb035bd90f4dd91
vivianluomin <273104241@qq.com>
2019-02-04 16:07:04 +0800

    JNI学习

--- NDKLearn/app/src/main/cpp/native-lib.cpp
+++ NDKLearn/app/src/main/cpp/native-lib.cpp
@@ -103,12 +103,184 @@ Java_com_example_asus1_ndklearn_MainActivity_accessStaticMathod(JNIEnv *env, job
     fclose(f);
 
     LOGD("output from C : File had saved", jstr);
-}extern "C"
+}
+
+
+extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_asus1_ndklearn_MainActivity_accessNonVirtualMathod(JNIEnv *env, jobject instance) {
 
-    // TODO
+    // 先拿到属性man
     jclass  clz = env->GetObjectClass(instance);
+    jfieldID  fid = env->GetFieldID(clz,"man","Lcom/example/asus1/ndklearn/Human;");
+    jobject  man = env->GetObjectField(instance,fid);
+
+    //拿到父类的类，以及speek的方法的id
+    jclass  clz_human = env->FindClass("com/example/asus1/ndklearn/Human");
+    jmethodID  mid = env->GetMethodID(clz_human,"speek","()V");
+
+    //调用自己的speek实现
+    env->CallVoidMethod(man,mid);
+
+    //调用父类的speek实现
+    env->CallNonvirtualVoidMethod(man,clz_human,mid);
+
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_accessConstructor(JNIEnv *env, jobject instance) {
+
+    jclass  clz_date = env->FindClass("java/util/Date");
+    //构造方法的函数名格式是：<init>
+    //不能写类名，因为构造方法函数名都一样区分不了，只能通过参数列表<签名>区分
+    jmethodID  mid_Date = env->GetMethodID(clz_date,"<init>","()V");
+
+    //调用构造函数
+    jobject  date = env->NewObject(clz_date,mid_Date);
+
+    //注意签名
+    jmethodID  mid_getTime = env->GetMethodID(clz_date,"getTime","()J");
+
+    //调用getTime方法
+    jlong  jtime = env->CallLongMethod(date,mid_getTime);
+
+    return jtime;
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_testChineseIn(JNIEnv *env, jobject instance,
+                                                           jstring chinese_) {
+    const char *chinese = env->GetStringUTFChars(chinese_, 0);
+
+    LOGD("%s",chinese);
+}
+
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_testChinsesOut(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+    char* c_str = "我爱你";
+    jstring  jstring1 = env->NewStringUTF(c_str);
+
+    return jstring1;
+}
+
+
+int compare(const void * a, const void * b)
+{
+    return (*(int *)a)-(*(int *)b);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_sortArray(JNIEnv *env, jobject instance,
+                                                       jintArray array_) {
+    //创建Java数组
+    //env->NewIntArray(len);
+
+    jint * c_arr = env->GetIntArrayElements(array_,NULL);
+    jsize len = env->GetArrayLength(array_);
+
+    //排序
+    qsort(c_arr,len, sizeof(jint),compare);
+
+    //操作完后需要同步C的数组到Java数组中
+    env->ReleaseIntArrayElements(array_,c_arr,0);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_localRef(JNIEnv *env, jobject instance) {
+
+    int i = 0;
+    for(;i<10;i++)
+    {
+        jclass  clz_date = env->FindClass("java/util/Date");
+        jmethodID  mid = env->GetMethodID(clz_date,"<init>","()V");
+        jobject  jobject_date = env->NewObject(clz_date,mid);
+
+        //此处省略一万行代码
+
+
+        //不再使用jobject对象
+        //通知垃圾回收器回收这些对象，确保内存充足
+        env->DeleteLocalRef(jobject_date);
+
+    }
+
+}
+
+//全局引用的字符串对象
+jstring global_str;
+
+//创建全局引用
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_createGlobalRef(JNIEnv *env, jobject instance) {
+
+    global_str = env->NewStringUTF("I LOVE YOU");
+    //通过NewGlobalRef创建全局引用
+    env->NewGlobalRef(global_str);
+
+
+}
+
+//获取全局引用
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_getGlobalRef(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return global_str;
+}
+
+
+//删除全局引用
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_deleteGlobalRef(JNIEnv *env, jobject instance) {
+
+    // 通过DelteGlobalRef删除全局变量
+    env->DeleteGlobalRef(global_str);
+
+}
+
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_asus1_ndklearn_MainActivity_testException(JNIEnv *env, jobject instance) {
+
+    jclass  clz = env->GetObjectClass(instance);
+    //属性名不小心写错了，拿到的是空的
+    jfieldID  fid = env->GetFieldID(clz,"key1","Ljava/lang/String;");
+
+    jthrowable  err = env->ExceptionOccurred();
+    if(err!=NULL)
+    {
+        env->ExceptionClear();
+        //提供补救措施，例如获取另外一个属性
+        fid = env->GetFieldID( clz, "key", "Ljava/lang/String;");
+    }
+
+
+    //LOGD("C can run,this will print");
+    //这里竟然还可以继续执行
+    jstring key = (jstring)(env->GetObjectField(instance,fid));
+    //遇到这句话的时候，C程序就Crash了
+    const char * c_str = env->GetStringUTFChars(key,NULL);
+
+    //LOGD("C Count not run,this will not print");
 
 
 }
--- NDKLearn/app/src/main/java/com/example/asus1/ndklearn/MainActivity.java
+++ NDKLearn/app/src/main/java/com/example/asus1/ndklearn/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.asus1.ndklearn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 
@@ -12,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+
     }
+
+    public String key = "qqq";
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +35,28 @@ public class MainActivity extends AppCompatActivity {
 //        accessStaticFiled();
 //        System.out.println(NUM);
 
-            accessMethd();
+           // accessMethd();
+
+        //accessNonVirtualMathod();
+
+//        long time = accessConstructor();
+//        System.out.println(time);
+
+//        testChineseIn("我爱你");
+//        System.out.println(testChinsesOut());
+
+//        int[] arr = {3,2,4,5,1,0};
+//        sortArray(arr);
+//        System.out.println(Arrays.toString(arr));
+
+        try {
+            testException();
+            Log.d(TAG, "程序无法继续执行时，这句话不会被打印\n");
+        }catch (Throwable t){
+            Log.d(TAG, "捕获到JNI抛出的异常（Throwable）："+t.getMessage()+"\n");
+        }
+
+        Log.d(TAG, "程序继续执行2，这句话会被打印\n");
     }
 
     /**
@@ -60,5 +88,30 @@ public class MainActivity extends AppCompatActivity {
 
     Human man = new Man();
     public native void accessNonVirtualMathod();
+
+    //调用Date的构造函数
+    public native long accessConstructor();
+
+    public native void testChineseIn(String chinese);//传进去
+    public native String testChinsesOut();//取出来
+
+    //数组处理
+    public native void sortArray(int array[]);
+
+
+    //局部引用
+    public native void localRef();
+
+    //创建JNI全局引用
+    public native void createGlobalRef();
+
+    //获取JNI全局引用
+    public native String getGlobalRef();
+
+    //删除JNI全局引用
+    public native void deleteGlobalRef();
+
+
+    public native void testException();
 
 }
